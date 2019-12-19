13b25c9e2c83528197f1d80099d45e7d2f72d8ad
UNO998 <shenyiran2015@outlook.com>
2019-12-19 16:23:25 -0500

    Adding api /tasks/showFormForAdd, /showFormForUpdate, /save

--- src/main/java/com/yiran/mtodoList/webController/TaskWebController.java
+++ src/main/java/com/yiran/mtodoList/webController/TaskWebController.java
@@ -2,13 +2,19 @@ package com.yiran.mtodoList.webController;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yiran.mtodoList.bean.Task;
 import com.yiran.mtodoList.service.TaskService;
@@ -37,5 +43,50 @@ public class TaskWebController {
 		theModel.addAttribute("tasks", theTasks);
 		
 		return "tasks/list-tasks";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Task theTask = new Task();
+		
+		theModel.addAttribute("task", theTask);
+		
+		return "tasks/task-form";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("taskId") Long theId,
+									Model theModel) {
+		
+		// get the task from the service
+		Task theTask = taskService.getTaskById(theId);
+		
+		// set task as a model attribute
+		theModel.addAttribute("task", theTask);
+		
+		return "tasks/task-form";			
+	}
+	
+	@PostMapping("/save")
+	public String addTask(
+			@ModelAttribute("task") @Valid Task theTask,
+			BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "tasks/task-form";
+		}
+		else {		
+			// save the task
+			if (theTask.getId() == null) {
+				taskService.addTask(theTask);
+			} else {
+				taskService.updateTask(theTask);
+			}
+
+			// use a redirect to prevent duplicate submissions
+			return "redirect:/tasks/list";
+		}
 	}
 }
